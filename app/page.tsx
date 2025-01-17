b771450e7f2dcab3ab1560c9dad483a1f942428d
Geesath Kannangara <skullmc101@gmail.com>
2025-01-17 22:39:31 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 // app/page.tsx
 import HeroSection from "./components/HeroSection";
+import SendToWebhook from "./components/SendToWebhook";
 
 export default function Home() {
   return (
     <div>
+      <SendToWebhook />
       <HeroSection />
     </div>
   );
